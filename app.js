cfc2d548d90678b4be183c17026b35e414989958
anammunoz <anamariamunozcarcel@gmail.com>
2022-08-23 19:40:46 +0000

    jest exercises

---
+++ app.js
@@ -0,0 +1,23 @@
+const sum = (a,b) => {
+    return a + b
+}
+
+// just a console log for ourselves.
+console.log(sum(7,3))
+
+// export the function to be used on other files 
+// (similar to the keyword `export` when using webpack)
+module.exports = { sum, fromEuroToDollar };
+
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDolar) {
+    let valueInYen = valueInDollar * 136,80
+}
+
+
